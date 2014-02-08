451f2ee238e0594537d6d87582c6aa434de010dc
Ben Freke <benfreke@gmail.com>
2014-02-08 21:35:44 +1000

    Initial release

--- README.md
+++ README.md
@@ -1,4 +1,57 @@
 bootstrap-collapse-clickchange
 ==============================
 
-Extends the Bootstrap Collapse plugin to allow automatic changing of both the target and parent element in the DOM.
+Extends the Bootstrap Collapse jQuery plugin to allow automatic changing of both the target and parent element in the DOM.
+
+**Version 1.0.0**
+
+# Usage
+
+Simply add `data-click="clickchange"` to the parent element.
+For groups, this is the parent element.
+For standalone collapses it is the clickable element.
+See the demo page for instructions and code.
+
+To have an individual collapse within a group behave differently, treat that collapse as a standalone.
+The standalone settings will completely over-ride any group settings, so these may need to be reset.
+
+The classes that are changed are toggled using jQuery's `toggleClass()`. Therefore both on and off classes need to be defined.
+
+### Example Code calling manually
+
+    $(document).ready(function () {
+        // Clickchange to all toggleable elements
+        $('[data-click="clickchange"]').each(function() {
+            if ($(this).data('toggle') != 'collapse') {
+                // It's a grouping, as I place the data on the parent element
+                var theParent = $(this);
+                theParent.find('[data-toggle="collapse"]').clickChange(null, theParent);
+            } else {
+                // This covers stand alone elements
+                $(this).clickChange();
+            }
+        });
+    });
+
+----
+
+# Options
+
+These options can be passed through data elements or directly on invocation. e.g. data-<option-name>
+
+* **when**: ( *before*|start) When to change trigger the changes. This will tie into the show/shown and hide/hidden events
+* **targetclass**: ( *null*|string) The classes to toggle on the container of the collapsible content.
+* **parentclass**: ( *null*|string) The classes to toggle on the clickable element that shows/hides the collapsible content.
+* **iconchange**: (true| *false*) Change icons within the clickable element
+* **iconclass**: ( *null*|string): The classes to change on the icon elements. e.g. `chevron-up chevron-down`
+* **iconprefix**: ( *glyphicon*|string) The icon prefix. Used to target which icons to change. e.g. The default will find all elements with a class of glyphicon to change
+* **iconprefixadd**: ( *true*|false) Whether to append this icon-prefix to the passed icon classes. If it is, a '-' character is added before the class names
+
+# To do
+
+* Create more examples
+*
+
+# Change Log
+
+No changes so far.
---
+++ bootstrap-collapse-clickchange.js
@@ -0,0 +1,105 @@
+/**
+ * Project: Bootstrap Hover Dropdown
+ * Author: Cameron Spear
+ *
+ * Dependencies: Bootstrap's Collapse plugin, jQuery
+ *
+ * A simple plugin to enable Bootstrap collapses to provide additional UX cues.
+ *
+ * License: GPL v2
+ */
+(function ( $ ) {
+
+    var ClickChange = function() {};
+
+    ClickChange.defaults = {
+        'when' : 'before',
+        'targetclass' : '',
+        'parentclass' : '',
+        'iconchange' : true,
+        'iconprefix' : 'glyphicon',
+        'iconprefixadd' : true,
+        'iconclass' : 'chevron-up chevron-down'
+    };
+
+    /**
+     * Set up the functions to fire on the bootstrap events
+     * @param options The options passed in
+     * @param controller Optional parent which controls a groups options
+     * @returns object For chaining
+     */
+    $.fn.clickChange = function ( options, controller ) {
+        // In a grouping, I've passed in the controller to get data from
+        var settings = $.extend(
+            {}
+            , ClickChange.defaults
+            , $(this).data()
+            , (typeof controller == 'object' && controller) ? controller.data() : {}
+            , typeof options == 'object' && options
+        );
+
+        // Now amend the icon class
+        if (settings.iconclass.length && settings.iconprefixadd) {
+            tmpArr = settings.iconclass.split(' ');
+            settings.iconclass = '';
+            tmpArr.forEach(function(element) {
+                settings.iconclass += ' ' + settings.iconprefix + '-' + element;
+            });
+            // Remove the first space, probably not necessary but it makes the code neater
+            settings.iconclass.trim();
+        }
+
+        // When do we fire the event?
+        var eventStart = (settings.when === 'after') ? 'shown' : 'show';
+        var eventEnd = (settings.when === 'after') ? 'hidden' : 'hide';
+
+        // Because it could be a group, we use each to iterate over the jQuery object
+        $(this).each(function(index, element) {
+            var clickElement = $(element);
+
+            // Get my target. This handles buttons and a
+            var clickTarget = clickElement.attr('data-target') || clickElement.attr('href');
+
+            // turn off previous events if we're re-initialising
+            if (clickElement.data('clickchange')) {
+                $(document).off(eventStart + '.bs.collapse ' + eventEnd + '.bs.collapse', clickTarget);
+            }
+            clickElement.data('clickchange', 'yes');
+
+            // As we're toggling, the same changes happen for both events
+            $(document).on(eventStart + '.bs.collapse ' + eventEnd + '.bs.collapse', clickTarget, function(event) {
+
+                // Toggle clickable element class?
+                if (settings.parentclass) {
+                    clickElement.toggleClass(settings.parentclass);
+                }
+
+                // Toggle the target class?
+                if (settings.targetclass) {
+                    $(event.target).toggleClass(settings.targetclass);
+                }
+
+                // Do I have icons to change?
+                if (settings.iconchange && settings.iconclass.length) {
+                    clickElement.find('.' + settings.iconprefix).toggleClass(settings.iconclass);
+                }
+            });
+        });
+        return this;
+    };
+
+    $(document).ready(function () {
+        // Clickchange to all toggleable elements
+        $('[data-click="clickchange"]').each(function() {
+            if ($(this).data('toggle') != 'collapse') {
+                // It's a grouping, as I place the data on the parent element
+                var theParent = $(this);
+                theParent.find('[data-toggle="collapse"]').clickChange(null, theParent);
+            } else {
+                // This covers stand alone elements
+                $(this).clickChange();
+            }
+        });
+    });
+
+}( jQuery ));
